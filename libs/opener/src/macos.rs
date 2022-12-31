97b3b46c0b180baab674e9c71cf40703897af109
xfqwdsj <xfqwdsj@qq.com>
2022-12-31 17:47:32 +0800

    fix(opener-macos): 尝试修复构建失败问题

--- libs/opener/src/macos.rs
+++ libs/opener/src/macos.rs
@@ -3,16 +3,27 @@ use std::ffi::OsStr;
 use std::process::{Command, Stdio};
 
 pub(crate) fn open(path: &OsStr, parameters: &OsStr) -> Result<(), OpenError> {
-    let mut open = Command::new("open").arg(path);
-    if !parameters.is_empty() {
-        open = open.arg("--args").arg(parameters);
-    }
-    open = open
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
-        .spawn()
-        .map_err(OpenError::Io)?;
+    if parameters.is_empty() {
+        let mut open = Command::new("open")
+            .arg(path)
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(OpenError::Io)?;
 
-    crate::wait_child(&mut open, "open")
+        crate::wait_child(&mut open, "open")
+    } else {
+        let mut open = Command::new("open")
+            .arg(path)
+            .arg("--args")
+            .arg(parameters)
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(OpenError::Io)?;
+
+        crate::wait_child(&mut open, "open")
+    }
 }
